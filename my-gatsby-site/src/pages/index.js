de5b5f387848f5d2d7ccb5f907322fe7d5fd3864
Thabo Nyatlo <thabo.nyatlo@live.com>
2022-04-04 12:51:27 +0200

    Use the <Link> component

--- my-gatsby-site/src/pages/index.js
+++ my-gatsby-site/src/pages/index.js
@@ -1,5 +1,7 @@
 // Step 1: Import React
 import * as React from 'react'
+// (5)
+import { Link } from 'gatsby'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -7,6 +9,7 @@ const IndexPage = () => {
     <main>
       <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
+      {/* (6) */}<Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   )
